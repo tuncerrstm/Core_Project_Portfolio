5297b68bde91b0800095013ae7354d5c3950c145
tuncerrstm <tuncerrstm@gmail.com>
2022-03-17 19:01:46 +0300

    Add : Portfolio Core Project

---
+++ Core_Project_Portfolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core_Project_Portfolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
